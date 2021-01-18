f220bacafdd6e201660432389b85616d3b2866dd
Marc CHANTERANNE <marc-chanteranne@outlook.fr>
2021-01-18 11:46:02 +0100

    Td BDD Symfony WP

---
+++ migrations/Version20210118094351.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210118094351 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE role (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, code VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, role_id INTEGER DEFAULT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE INDEX IDX_8D93D649D60322AC ON user (role_id)');
+        $this->addSql('DROP INDEX IDX_6BAF78709F8008B6');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__ingredient AS SELECT id, cake_id, name, is_allergen FROM ingredient');
+        $this->addSql('DROP TABLE ingredient');
+        $this->addSql('CREATE TABLE ingredient (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cake_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL COLLATE BINARY, is_allergen BOOLEAN NOT NULL, CONSTRAINT FK_6BAF78709F8008B6 FOREIGN KEY (cake_id) REFERENCES cake (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO ingredient (id, cake_id, name, is_allergen) SELECT id, cake_id, name, is_allergen FROM __temp__ingredient');
+        $this->addSql('DROP TABLE __temp__ingredient');
+        $this->addSql('CREATE INDEX IDX_6BAF78709F8008B6 ON ingredient (cake_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP INDEX IDX_6BAF78709F8008B6');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__ingredient AS SELECT id, cake_id, name, is_allergen FROM ingredient');
+        $this->addSql('DROP TABLE ingredient');
+        $this->addSql('CREATE TABLE ingredient (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cake_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, is_allergen BOOLEAN NOT NULL)');
+        $this->addSql('INSERT INTO ingredient (id, cake_id, name, is_allergen) SELECT id, cake_id, name, is_allergen FROM __temp__ingredient');
+        $this->addSql('DROP TABLE __temp__ingredient');
+        $this->addSql('CREATE INDEX IDX_6BAF78709F8008B6 ON ingredient (cake_id)');
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Role;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Repository\RoleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController
+{
+    /**
+     * @Route ("/createUser", name="createUser")
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function createUser(EntityManagerInterface $entityManager):Response {
+        $user = new User();
+        $user->setLogin('JP4');
+        $user->setPassword('jp4psswd');
+        $user->setFirstname('Jean-Pierre');
+        $user->setLastname('Rammus');
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return new Response('Saved new user with id'.$user->getId());
+    }
+
+    /**
+     * @Route ("/createRole", name="createRole")
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function createRole(EntityManagerInterface $entityManager):Response {
+        $role = new Role();
+        $role->setName('tester');
+        $role->setCode('3');
+        $entityManager->persist($role);
+        $entityManager->flush();
+        return new Response('Saved new role with id'.$role->getId());
+    }
+
+    public function attributeRole(EntityManagerInterface $entityManager ){
+
+    }
+}
